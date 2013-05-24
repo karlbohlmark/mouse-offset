5e6bb11830db08b70b4d1b2265419d5e1bd04aea
Karl Böhlmark <karl.bohlmark@edgeware.tv>
2013-05-24 11:34:29 +0200

    change x,y -> left,top.  add example

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var offset = require('offset');
+var elementOffset = function(elem) {
+  var x, y;
+  x = 0;
+  y = 0;
+  while (true) {
+    x += elem.offsetLeft;
+    y += elem.offsetTop;
+    if (!(elem = elem.offsetParent)) {
+      break;
+    }
+  }
+  return {
+    x: x,
+    y: y
+  };
+};
 
-var getOffset = function(elem, mouseEvent) {
+var getOffset = function(elem, event) {
     var offset = elementOffset(elem);
     return {
-        x: event.pageX - offset.x,
-        y: event.pageY - offset.y
+        left: event.pageX - offset.x,
+        top: event.pageY - offset.y
     };
 };
 
